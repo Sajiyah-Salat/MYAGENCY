b280c84085f45143e4bef8a0517dbe9fd5d61cdc
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2025-03-25 15:57:06 +0530

    add

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -13,8 +13,8 @@ export async function POST(req) {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: `salatsajiya7.8.6@gmail.com`,
+        pass: `apoy dzyo tdca gatg`,
       },
     });
 
